3e0c1d4e763500f9fae8cee984d9562c6eb3e123
AayushAKT <116262606+AayushAKT@users.noreply.github.com>
2024-09-01 19:48:49 +0530

    Add files via upload

---
+++ movie-recommender.py
@@ -0,0 +1,35 @@
+import pickle
+import streamlit as st
+import requests
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    for i in distances[1:6]:
+        # fetch the movie poster
+        movie_id = movies.iloc[i[0]].id
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+
+    return recommended_movie_names
+
+
+st.header('Movie Recommender System')
+movies = pickle.load(open('movies.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+movie_list = movies['title'].values
+selected_movie = st.selectbox(
+    "Type or select a movie from the dropdown",
+    movie_list
+)
+
+if st.button('Show Recommendation'):
+    recommended_movie_names = recommend(selected_movie)
+
+    for x in recommended_movie_names:
+        st.write(x)
+
+
+
+
